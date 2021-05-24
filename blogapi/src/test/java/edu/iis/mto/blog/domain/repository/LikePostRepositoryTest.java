d4320cb8aa7614427691c877fda03335a8e50b3d
krecikovsky112 <krecikovsky112@gmail.com>
2021-05-24 14:20:41 +0200

    Create LikePostRepositoryTest

---
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
@@ -0,0 +1,61 @@
+package edu.iis.mto.blog.domain.repository;
+
+import edu.iis.mto.blog.domain.model.AccountStatus;
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.model.LikePost;
+import edu.iis.mto.blog.domain.model.User;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class LikePostRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private LikePostRepository repository;
+
+    private User user;
+    private BlogPost blogPost;
+    private LikePost likePost;
+
+    @Before
+    public void setUp() {
+        user = new User();
+        user.setFirstName("Jan");
+        user.setLastName("Kowalski");
+        user.setEmail("john@domain.com");
+        user.setAccountStatus(AccountStatus.NEW);
+
+        blogPost = new BlogPost();
+        blogPost.setEntry("test");
+        blogPost.setUser(user);
+
+        likePost = new LikePost();
+        likePost.setPost(blogPost);
+        likePost.setUser(user);
+
+        entityManager.persist(user);
+        entityManager.persist(blogPost);
+    }
+
+    @Test
+    public void shouldFindNoLikePostsIfRepositoryIsEmpty() {
+        List<LikePost> likePosts = repository.findAll();
+
+        assertThat(likePosts, hasSize(0));
+    }
+
+}
